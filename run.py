7d6ed11a3c285fbc2783be8fe0509fd4bb1e1f8c
vs001 <vs001@cox.net>
2023-03-08 21:14:43 -0700

    save nav stuff

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from flask_migrate import Migrate
 
 from app_init      import db, login, appRoutes
 
-app = Flask(__name__, template_folder='app/templates', static_folder=os.path.join(os.getcwd(),'app','static'))
+#app = Flask(__name__, template_folder='app/templates', static_folder=os.path.join(os.getcwd(),'app','static'))
+app = Flask(__name__, template_folder='app/templates')
 app.config.from_object(Config)
 
 db.init_app(app)
