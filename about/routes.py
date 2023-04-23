87d63eeb2c5cd9a1a373cbc8a09622772b88cffb
vs001 <vs001@cox.net>
2023-04-23 12:06:57 -0700

    save wip

---
+++ about/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+about_bp = Blueprint(
+    'about_bp', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+@about_bp.route('/about')   # Focus here
+def about_home():
+    return render_template('login.html', title='About')
